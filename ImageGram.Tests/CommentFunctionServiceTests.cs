6c34ac4126562ae036a230a0a3ea5958e5793f18
Andreanus Agung Purnomo <>
2022-10-02 22:55:34 +0700

    Add unit tests

---
+++ ImageGram.Tests/CommentFunctionServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using ImageGram.Application.Interfaces;
+using ImageGram.Application.RequestModels;
+using ImageGram.Application.Services;
+using ImageGram.Domain;
+
+namespace ImageGram.Tests;
+
+public class CommentFunctionServiceTests
+{
+    static readonly Fixture Fixture = new Fixture();
+
+    private ICommentFunctionService _commentFunctionService;
+    private Mock<ICommentRepository> mockCommentRepository = new Mock<ICommentRepository>();
+
+    [Fact]
+    public async void SaveCommentAsync_ReturnsComment()
+    {
+        var commentRequestModel = Fixture.Create<CommentRequestModel>();
+
+        mockCommentRepository
+            .Setup(s => s.CreateCommentAsync(It.IsAny<Comment>()))
+            .Returns(Task.CompletedTask);
+
+        _commentFunctionService = new CommentFunctionService(mockCommentRepository.Object);
+
+        var result = await _commentFunctionService.SaveCommentAsync(commentRequestModel);
+
+        mockCommentRepository.Verify(r => r.CreateCommentAsync(It.IsAny<Comment>()), Times.Once);
+
+        result.Should().BeOfType<Comment>();
+        result.CommentText.Should().Be(commentRequestModel.commentText);
+        result.PostId.Should().Be(commentRequestModel.postId);
+        result.UserId.Should().Be(commentRequestModel.userId);
+    }
+
+    [Fact]
+    public async void DeleteCommentAsync_CallsDeleteCommentAsyncOnce()
+    {
+        mockCommentRepository
+            .Setup(s => s.DeleteCommentAsync(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(Task.CompletedTask);
+
+        _commentFunctionService = new CommentFunctionService(mockCommentRepository.Object);
+
+        await _commentFunctionService.DeleteCommentAsync(It.IsAny<string>(), It.IsAny<string>());
+
+        mockCommentRepository.Verify(r => r.DeleteCommentAsync(It.IsAny<string>(), It.IsAny<string>()));
+    }
+}
---
+++ ImageGram.Tests/PostFunctionServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using ImageGram.Application.Interfaces;
+using ImageGram.Application.RequestModels;
+using ImageGram.Application.Services;
+using ImageGram.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+
+namespace ImageGram.Tests;
+
+public class PostFunctionServiceTests
+{
+    static readonly Fixture Fixture = new Fixture();
+
+    private IPostFunctionService _postFunctionService;
+    private Mock<IPostRepository> mockPostRepository = new Mock<IPostRepository>();
+    private Mock<IImageUploader> mockImageUploader = new Mock<IImageUploader>();
+
+    [Fact]
+    public async void SavePostAsync_ReturnsPost()
+    {
+
+        //Setup mock file using a memory stream
+        var content = "Mock Content";
+        var fileName = "mock.png";
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(content);
+        writer.Flush();
+        stream.Position = 0;
+
+        //create FormFile with desired data
+        IFormFile file = new FormFile(stream, 0, stream.Length, "id_from_form", fileName);
+
+        var postRequestModel = new PostRequestModel(Fixture.Create<string>(), Fixture.Create<string>(), file);
+
+        mockPostRepository
+            .Setup(s => s.CreatePostAsync(It.IsAny<Post>()))
+            .Returns(Task.CompletedTask);
+
+        mockImageUploader
+            .Setup(s => s.UploadImage(It.IsAny<IFormFile>(), It.IsAny<string>()))
+            .Returns(Task.CompletedTask);
+
+        _postFunctionService = new PostFunctionService(mockPostRepository.Object, mockImageUploader.Object);
+
+        var result = await _postFunctionService.SavePostAsync(postRequestModel);
+
+        mockPostRepository.Verify(r => r.CreatePostAsync(It.IsAny<Post>()), Times.Once);
+        mockImageUploader.Verify(r => r.UploadImage(It.IsAny<IFormFile>(), It.IsAny<string>()));
+
+        result.Should().BeOfType<Post>();
+        result.Caption.Should().Be(postRequestModel.caption);
+        result.UserId.Should().Be(postRequestModel.userId);
+        result.ImageUrl.Should().Contain(".jpg");
+    }
+}
